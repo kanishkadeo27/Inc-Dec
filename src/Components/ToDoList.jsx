9d399e60e2db6a4818c676fd99715d55b3c6b44c
kanishkadeo27 <Kanishkadeo92022@gmail.com>
2024-01-24 18:08:14 +0530

    added material ui tooltip

--- src/Components/ToDoList.jsx
+++ src/Components/ToDoList.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { useState } from "react";
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
 
 function ToDoList() {
   const [num, setNum] = useState(0);
@@ -23,8 +25,19 @@ function ToDoList() {
         <div className="center-div">
           <h1>{num}</h1>
           <div className="button-div">
-            <button onClick={incFn}><AddIcon/></button>
-            <button onClick={decFn}><RemoveIcon/></button>
+            <Tooltip title="Increment" className="tootip">
+            <Button onClick={incFn} className="btn-green"><AddIcon/></Button>
+            </Tooltip>
+            <Tooltip title="Decrement" className="tooltip">
+            <Button onClick={decFn} className="btn-red"><RemoveIcon/></Button>
+            </Tooltip>
+
+
+            {/* <Button variant="contained" onClick={incFn}><AddIcon/></Button>
+            <Button variant="contained" onClick={decFn}><RemoveIcon/></Button> */}
+            {/* <button onClick={incFn}><AddIcon/></button>
+            <button onClick={decFn}><RemoveIcon/></button> */}
+
           </div>
         </div>
       </div>
